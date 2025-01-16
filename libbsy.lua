b191e7c9571eb0af5698c8645cbdcb4038a27b5d
PurplMochii <98187845+ghastlytim@users.noreply.github.com>
2025-01-16 11:43:58 -0500

    Update libbsy.lua

--- libbsy.lua
+++ libbsy.lua
@@ -1,5 +1,5 @@
 local Solaris = Instance.new("ScreenGui")
-Solaris.Name = "dosage's solaris gui"
+Solaris.Name = "pornhub.com"
 Solaris.Parent = game.CoreGui
 Solaris.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
